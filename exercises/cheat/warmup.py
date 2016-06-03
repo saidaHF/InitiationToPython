1e836c5db865e41118047dbd5b6564a8ccfc8972
cpascual <cpascual@cells.es>
2016-06-03 08:57:23 +0200

    Add new file

---
+++ exercises/cheat/warmup.py
@@ -0,0 +1,38 @@
+"""
+Warm-up exercises
+-----------------
+
+Simple exercises to get used to python basics.
+
+- From http://introtopython.org/var_string_num.html , do the following:
+  - hello world
+  - one variable, two messages
+  - first name cases
+  - full name
+  - arithmetic
+  
+- From http://introtopython.org/lists_tuples.html , do the following:
+  - first list
+  - working list
+  - starting from empty
+  - ordered numbers (ignore the requirement of using for loops)
+  - alphabet slices
+  - first twenty
+  
+- And *also* the following exercise on dictionaries:
+  Create the following dictionary to be used as a phonebook:
+  
+        phonebook = { "John" : 7566,  "Jack" : 7264,  "Jill" : 2781}
+
+  Add "Jake" to the phonebook with the phone number 938273443,  and remove Jill
+  from the phonebook. Then print the phonebook in alphabetical order
+"""
+
+# Write your solution here
+
+# Really???? 
+# You want to cheat on the warm-up exercises?
+# c'mon. Just go back, and read:
+# - https://docs.python.org/2/tutorial/introduction.html
+# - http://introtopython.org/var_string_num.html
+# - http://introtopython.org/lists_tuples.html
